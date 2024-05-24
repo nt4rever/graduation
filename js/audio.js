b74191658869cb450566e9780a66700f4dd4b97b
tan.le <tan.le@napaglobal.com>
2024-05-24 16:09:43 +0700

    add second music

--- js/audio.js
+++ js/audio.js
@@ -1,43 +1,44 @@
 export const audio = (() => {
+  const music = document.getElementById("button-music");
+  let audio = null;
+  const audioSrc = ["./assets/music/sound.mp3", "./assets/music/audio.mp3"];
 
-    const music = document.getElementById('button-music');
-    let audio = null;
+  const getAudio = () => {
+    if (!audio) {
+      audio = new Audio();
+    //   audio.src = music.getAttribute("data-url");
+      audio.src = audioSrc[Math.floor(Math.random() * 2)];
+      audio.load();
+      audio.currentTime = 0;
+      audio.autoplay = true;
+      audio.muted = false;
+      audio.loop = true;
+      audio.volume = 1;
+    }
 
-    const getAudio = () => {
-        if (!audio) {
-            audio = new Audio();
-            audio.src = music.getAttribute('data-url');
-            audio.load();
-            audio.currentTime = 0;
-            audio.autoplay = true;
-            audio.muted = false;
-            audio.loop = true;
-            audio.volume = 1;
-        }
+    return audio;
+  };
 
-        return audio;
-    };
+  const button = (button) => {
+    if (button.getAttribute("data-status") !== "true") {
+      button.setAttribute("data-status", "true");
+      getAudio().play();
+      button.innerHTML = '<i class="fa-solid fa-circle-pause spin-button"></i>';
+      return;
+    }
 
-    const button = (button) => {
-        if (button.getAttribute('data-status') !== 'true') {
-            button.setAttribute('data-status', 'true');
-            getAudio().play();
-            button.innerHTML = '<i class="fa-solid fa-circle-pause spin-button"></i>';
-            return;
-        }
+    button.setAttribute("data-status", "false");
+    getAudio().pause();
+    button.innerHTML = '<i class="fa-solid fa-circle-play"></i>';
+  };
 
-        button.setAttribute('data-status', 'false');
-        getAudio().pause();
-        button.innerHTML = '<i class="fa-solid fa-circle-play"></i>';
-    };
+  const showButton = () => {
+    music.style.display = "block";
+  };
 
-    const showButton = () => {
-        music.style.display = 'block';
-    };
-
-    return {
-        play: () => getAudio().play(),
-        button,
-        showButton,
-    };
+  return {
+    play: () => getAudio().play(),
+    button,
+    showButton,
+  };
 })();
